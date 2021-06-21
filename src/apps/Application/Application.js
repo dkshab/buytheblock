82ac708b8372247426c3ff1892322e25adeebc1d
Dumisani <dumisanik.shabangu@gmail.com>
2021-06-21 19:47:36 +0200

    initial bootstrap

---
+++ src/apps/Application/Application.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "../../sass/styles.scss";
+
+import NonAuthApp from "../NonAuthApp/NonAuthApp";
+
+function Application() {
+  return (
+    <div className="">
+      <NonAuthApp />
+    </div>
+  );
+}
+
+export default Application;
---
+++ src/apps/NonAuthApp/NonAuthApp.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import NavBarNonAuth from "./static/NavBarNonAuth/NavBarNonAuth";
+
+const NonAuthApp = () => {
+  return (
+    <div className="NonAuthApp">
+      <NavBarNonAuth />
+      Non Auth Baby!
+    </div>
+  );
+};
+
+export default NonAuthApp;
